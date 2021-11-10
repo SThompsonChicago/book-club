cb50d81da97009464e23f7bb96a0822f1c98816f
CJMc0d3 <cammcd35@gmail.com>
2021-11-10 14:57:03 -0600

    added title to the model and review json and rendered review title and rating on home page

--- models/Review.js
+++ models/Review.js
@@ -12,6 +12,10 @@ Review.init(
       primaryKey: true,
       autoIncrement: true,
     },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     content: {
       type: DataTypes.STRING,
       allowNull: false,
