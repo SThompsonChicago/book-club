cb57fa5481fd821bc24f6b5dca8849d39d87d2fe
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2021-11-11 11:20:58 -0600

    dayjs current time works on homepage

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,7 @@
 const dayjs = require('dayjs');
 
 module.exports = {
-  date_created: (date) => {
-    return dayjs(date).format('MM-DD-YYYY')
-  }
+format_date: () => {
+  return dayjs().format('MM-DD-YYYY')
+}
 };
