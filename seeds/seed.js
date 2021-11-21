869399552bb9b9c061ef3b04030e9ca507bdaeac
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2021-11-21 10:13:27 -0600

    comment seeds set up and added to seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,9 +1,10 @@
 const sequelize = require('../config/connection');
-const { User, Review, Book } = require('../models');
+const { User, Review, Book, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const reviewData = require('./reviewData.json');
 const bookData = require('./bookData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   try {
@@ -17,6 +18,8 @@ const seedDatabase = async () => {
     await Book.bulkCreate(bookData);
 
     await Review.bulkCreate(reviewData);
+
+    await Comment.bulkCreate(commentData);
 
   } catch (err) {
     console.error(err);
