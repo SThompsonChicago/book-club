4c6cc20f5b1df90105075fbfbcc18e71a426aa15
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2021-11-21 12:46:48 -0600

    signup form saves information.  Able to log out and log back in with signed up information

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,26 +1,34 @@
-const signupFormHandler = async function(event) {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-input-signup').value.trim();
-    const password = document.querySelector('#password-input-signup').value.trim();
-    const first_name = document.querySelector('#first_name-input-signup').value.trim();
-    const last_name = document.querySelector('#last_name-input-signup').value.trim();
+const signupFormHandler = async (event) => {
+  event.preventDefault();
 
+  const email = document.querySelector('#email-input-signup').value.trim();
+  const password = document.querySelector('#password-input-signup').value.trim();
+  const first_name = document.querySelector('#first_name-input-signup').value.trim();
+  const last_name = document.querySelector('#last_name-input-signup').value.trim();
+
+  console.log(email, password, first_name, last_name);
+
+  if (email && password && first_name && last_name) {
     const response = await fetch('/api/users/signup', {
       method: 'POST',
       body: JSON.stringify({
-        first_name, last_name, email, password
+        "email": email,
+        "password": password,
+        "first_name": first_name,
+        "last_name": last_name,
       }),
       headers: { 'Content-Type': 'application/json' },
     });
-  
+    console.log(response);
+
     if (response.ok) {
       document.location.replace('/');
     } else {
-      alert('Failed to sign up');
+      alert(response.statusText);
     }
-  };
-  
-  document
-    .querySelector('#signup-submit')
-    .addEventListener('click', signupFormHandler);
+  }
+};
+
+document
+  .querySelector('#signup-form')
+  .addEventListener('submit', signupFormHandler);
